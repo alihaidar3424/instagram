0f1cc2158c8b127df30b03b80605ef22ad326f45
Ali Haidar <ali.haidar@devsinc.com>
2021-09-27 19:08:56 +0500

    USer Profile: USer controller added & show user profile view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
 
   root 'pages#home'
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
