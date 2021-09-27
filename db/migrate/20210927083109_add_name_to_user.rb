1f1a4adc3d8c817907df83362ede74f81539fa06
Ali Haidar <ali.haidar@devsinc.com>
2021-09-27 15:09:36 +0500

    User: db level validation on name field

--- db/migrate/20210927083109_add_name_to_user.rb
+++ db/migrate/20210927083109_add_name_to_user.rb
@@ -2,6 +2,6 @@
 
 class AddNameToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :name, :string
+    add_column :users, :name, :string, presence: true, length: { in: 3..50 }
   end
 end
