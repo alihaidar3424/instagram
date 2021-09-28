268e1990d6032d0f176a2ddafb1f1daf041a1f40
Ali Haidar <ali.haidar@devsinc.com>
2021-09-28 12:03:16 +0500

    User Profile: resolving update user profile issues

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
+    puts params
     resource.update_without_password(params)
   end
 end
