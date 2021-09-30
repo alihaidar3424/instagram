bace8af757651dddd5c506600f51ff894705ca63
Ali Haidar <ali.haidar@devsinc.com>
2021-09-30 16:18:22 +0500

    extra route removed

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
              path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' },
              controllers: { registrations: 'registrations' }
   resources :users, only: [:show]
-  resources :posts
+  resources :posts, except: %i[edit update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
