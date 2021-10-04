a282c2425437344e35a64c19aa4cc524ac72b4e3
Ali Haidar <ali.haidar@devsinc.com>
2021-10-04 20:26:17 +0500

    Comment-feature: create, index & destroy comment action implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,33 @@
+# frozen_string_literal: true
 class CommentsController < ApplicationController
+  def index
+    @comments = @post.comments.includes(:user)
+  end
 
+  def create
+    comment = current_user.comments.build(comment_params)
+    @post = comment.post
+    if comment.save
+      respond_to :js
+    else
+      flash[:alert] = 'Something went wrong ...'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by id: params[:id]
+    authorize @comment
+    @post = @comment.post
+    if @comment.destroy
+      respond_to :js
+    else
+      flash[:alert] = 'Something went wrong ...'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.required(:comment).permit :user_id, :post_id, :content
+  end
 end
