04e5bd8c4f6aaf6be19ca133044dd8126ef7dee8
Ali Haidar <ali.haidar@devsinc.com>
2021-10-06 12:43:39 +0500

    Followership: follow to each other user functionallity is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @followed = current_user.followers.pluck(:id).include?(@user.id)
+  end
+
+  def follow_user
+    # following_relationships.create(following_id: user_id)
+    @followed = Follow.create(follower_id: params[:follow_id], following_id: current_user.id)
+  end
+
+  def unfollow_user
+    # following_relationships.find_by!(following_id: user_id).destroy
+    @followed = Follow.find_by!(follower_id: params[:follow_id], following_id: current_user.id).destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,16 @@ Rails.application.routes.draw do
              path: '',
              path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' },
              controllers: { registrations: 'registrations' }
-  resources :users, only: [:show]
+
+  resources :users, only: [:show] do
+    post 'follow', to: 'users#follow_user', as: :follow, on: :collection
+    post 'unfollow', to: 'users#unfollow_user', as: :unfollow, on: :collection
+  end
+
   resources :posts do
     resources :likes, only: %i[create destroy], shallow: true
     resources :comments, except: %i[new show], shallow: true
+    resources :followership, only: %i[create destroy], shallow: true
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
