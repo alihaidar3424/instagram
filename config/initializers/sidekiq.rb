c1e4a638c32e1e48b31fd4993eecb33c540e6e66
Ali Haidar <ali.haidar@devsinc.com>
2021-10-12 12:34:54 +0500

    Job: added default job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 schedule_file = 'config/schedule.yml'
-Sidekiq::Queue.all.each(&:clear)
 Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
